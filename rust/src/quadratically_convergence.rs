c269372c3cf22ce7ff2bc60eb929a393f88b69cb
manas <66103885+manasbir@users.noreply.github.com>
2022-09-26 15:20:59 -0700

    a

--- rust/src/quadratically_convergence.rs
+++ rust/src/quadratically_convergence.rs
@@ -4,6 +4,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use ethers::types::U256;
+use csv;
 mod quad_conv_proof;
 mod guessing_methods;
 
@@ -12,18 +13,18 @@ fn main() {
     let mut i: U256 = U256::MAX>>128;
     let mut string = String::new();
     //let mut string2 = String::new();
-    let mut file = File::create("quad_conv_wo_guess.txt").unwrap();
+    let mut file = File::create("quad_conv_wo_guess.csv").unwrap();
     string.push_str(&format!("i, count, iterations, iterations_in_binary \n"));
 
-    while i < U256::MAX/1.5.into() {
+    while i < U256::MAX {
         let (x, iterations1) = quad_conv_proof::newton_sqrt(i, i);
         // ignoring the my super swag method for now
         //let (y, iterations2) = quad_conv_proof::newton_sqrt(i, guessing_methods::oz_msb(i));
 
         string.push_str(&format!("{}, {}, \n", i, iterations1.len()).to_string());
 
-        let iterations = iterations1.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(", ");
-        let iterations_in_binary = iterations1.iter().map(|x| format!("{:b}", x)).collect::<Vec<String>>().join(", ");
+        let iteratiosn = iterations1.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(", ");
+        let iterations_in_binary = iterations1.into_iter().map(|x| {format!("{x:b}")}).collect::<Vec<_>>().join(", ");
         // copilot is godsend
 
         // todo:
@@ -32,7 +33,7 @@ fn main() {
         // do the same but with correct # of digits rather than numerically
         // do the same in binary
 
-        string.push_str(&format!("{iterations}\n").to_string());
+        string.push_str(&format!("{iterations1}\n").to_string());
         string.push_str(&format!("{iterations_in_binary}\n\n").to_string());
 
         i/=2;
